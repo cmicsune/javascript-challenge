7a94c50f8ab6d876ffe00731125b63a5369691a7
Cora Micsunescu <csmicsun@icloud.com>
2021-01-26 08:59:58 -0800

    create javascript table

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
+d3.select('button').on('click', handleClick);
 
-// YOUR CODE HERE!
+function showTable (data) {
+    d3.select('tbody').html('');
+    data.forEach(obj => {
+        var row= d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val=>{
+            var cell= row.append('td');
+            cell.text(val);
+
+        });
+        
+    });
+};
+
+function handleClick() {
+    var filterData=data;
+    var value= d3.select('input').property('value');
+    if (value) {
+        filterData=filterData.filter(obj=> obj.datetime == value);
+    };
+    d3. select('input').property('value', '');
+    showTable(filterData);
+};
+
